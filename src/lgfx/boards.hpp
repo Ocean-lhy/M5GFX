ee334939a5781246eec95b5e8c5ede2718266127
Tinyu <tinyu0806@gmail.com>
2026-03-11 11:35:53 +0800

    Add StampS3Bat board id.

--- src/lgfx/boards.hpp
+++ src/lgfx/boards.hpp
@@ -58,6 +58,7 @@ namespace lgfx // This should not be changed to "m5gfx"
     , board_M5PowerHub
     , board_M5DualKey
     , board_M5UnitPoEP4
+    , board_M5StampS3Bat
 
 /// external displays
     , board_M5AtomDisplay = 192
